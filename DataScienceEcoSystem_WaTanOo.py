612b3a27b25cdc939bb32ff1262f3ee11eac9498
thitsar7 <139748202+thitsar7@users.noreply.github.com>
2023-12-16 21:08:50 -0500

    Add files via upload

---
+++ DataScienceEcoSystem_WaTanOo.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# #### Some of the popular languages that Data Scientists use are:
+# 
+# 1.  Python
+# 2.  SQL
+# 3.  R
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1.  TensorFlow.
+# 2.  SciPy.
+# 3.  NumPy.
+# 4.  Pandas.
+# 5.  Pytorch
+
+# |Data Science Tools|
+# |---|
+# |SAS|
+# |Apache Hadoop|
+# |TensorFlow|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+print(200 / 6)
+
+
+# #### Objectives:
+
+# * Python
+# * SQL
+# * R
+
+# ## Author
+# Wa Tan Oo
